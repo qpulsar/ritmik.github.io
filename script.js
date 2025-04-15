d36627a9f3eff4fed6665eff59a3def7252af730
qpulsar <korkusuz@balikesir.edu.tr>
2025-04-15 11:12:15 +0300

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+function startGame(step) {
+    console.log(`Başladı: ${step}'er sayma oyunu`);
+    alert(`${step}'er sayma oyunu yakında başlayacak!`);
+    // Buraya oyun sayfasına yönlendirme veya içerik yükleme gelebilir
+    // Örnek: window.location.href = `oyun-${step}.html`;
+}
